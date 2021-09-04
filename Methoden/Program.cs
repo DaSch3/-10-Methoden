1b4c32f0e64f270f2dc75609d04e0f90105a13dc
David Schmitz <d.schmitz@dasch3.de>
2021-09-04 19:28:13 +0200

    Projektdateien hinzufügen.

---
+++ Methoden/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methoden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Begrüßung("Peter");
+            Begrüßung("Janek");
+            Console.ReadKey();
+        }
+
+        static void Begrüßung(string name)
+        {
+            Console.WriteLine("Hallo " + name);
+        }
+    }
+}
